7ff7a6a0722766ca55ebb7058fc52644c3cd46a0
varunsh17 <varunsh170502@gmail.com>
2022-04-10 19:01:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3> Register User </h3>
+        <input placeholder="Email..." />
+        <input placeholder="Password..." />
+
+        <button > Create User</button>
+      </div>
+
+      <div>
+        <h3> Login </h3>
+        <input placeholder="Email..." />
+        <input placeholder="Password..." />
+
+        <button > Login</button>
+      </div>
+
+      <h4> User Logged In: </h4>
+      <button > Sign Out </button>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Components/firebase.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+// function homepage(props) {
+//     return (
+//         <div>
+//             {props.email}
+
+//         </div>
+//     )
+// }
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrFfeJkAsT-SFFtUe63NntCUV9ChPhShg",
+    authDomain: "fir-2f124.firebaseapp.com",
+    projectId: "fir-2f124",
+    storageBucket: "fir-2f124.appspot.com",
+    messagingSenderId: "1070217333815",
+    appId: "1:1070217333815:web:9bdf3ebd6b5e6ba25203b2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
